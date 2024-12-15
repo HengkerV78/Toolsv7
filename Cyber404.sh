0a4a23727bd81e2c31f053f35ccead0a32ddfa41
HengkerV78 <luubotakluukren432@gmail.com>
2024-12-15 15:32:25 +0700

    Toolsv7

---
+++ Cyber404.sh
@@ -0,0 +1,273 @@
+#!/bin/bash
+
+# Warna
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+# Persiapan
+clear
+termux-setup-storage -y 
+clear
+pkg install python python2 python3 -y
+pip install psutil
+
+# Banner
+echo -e "${red}
+           
+⠤⣤⣤⣤⣄⣀⣀⣀⣀⣀                             ⣀⣀⣠⣤⠤⠤⠴⠶⠶⠶⠶
+⢠⣤⣤⡄⣤⣤⣤⠄⣀⠉⣉⣙⠒⠤⣀                    ⣠⠴⠘⣉⢡⣤⡤⠐⣶⡆⢶ ⣶⣶⡦
+⣄⢻⣿⣧⠻⠇⠋ ⠋ ⢘⣿⢳⣦⣌⠳⠄                 ⠞⣡⣴⣧⠻⣄⢸⣿⣿⡟⢁⡻⣸⣿⡿⠁
+⠈⠃⠙⢿⣧⣙⠶⣿⣿⡷⢘⣡⣿⣿⣿⣷⣄                ⢠⣾⣿⣿⣿⣷⣝⡳⠶⠶⠾⣛⣵⡿⠋  
+    ⠉⠻⣿⣶⠂⠘⠛⠛⠛⢛⡛⠋⠉                    ⠉⠉⠉⠛ ⠉⠒⠛     
+      ⣿⡇     ⢸⠃                                   
+      ⣿⡇     ⣾                                    
+      ⣿⡇     ⣿                                    
+      ⢻⡁     ⢸                                    
+      ⠘⡇                                          
+       ⡇                                          
+       ⠿                      
+"
+echo -e "${purple}======================================"
+echo -e "${red}       ™Mode,in By.Thonxyzz404™"
+echo -e "${purple}======================================"
+echo -e "${blue}➣ ${green}[01]◉ SPAM PAIRV1${blue}"
+echo -e "${blue}➣ ${green}[02]◉ SPAM PAIRV2${blue}"
+echo -e "${blue}➣ ${green}[03]◉ SPAM PAIRV3${blue}"
+echo -e "${blue}➣ ${green}[04]◉ SPAM PAIRV4${blue}"
+echo -e "${blue}➣ ${green}[05]◉ SPAM PAIRV5${blue}"
+echo -e "${yellow}➣ ${yellow}[06]◉ SPAM PAIRV6${blue}"
+echo -e "${yellow}➣ ${yellow}[07]◉ OSINT-NOMER${blue}"
+echo -e "${yellow}➣ ${yellow}[08]◉ OSINT-TRACK${blue}"
+echo -e "${yellow}➣ ${yellow}[09]◉ ZPHISHER${blue}"
+echo -e "${yellow}➣ ${yellow}[10]◉ OSINT-NIK${blue}"
+echo -e "${green}➣ ${cyan}[11]◉ Followers${blue}"
+echo -e "${green}➣ ${cyan}[12]◉ All-OSINT${blue}"
+echo -e "${green}➣ ${cyan}[13]◉ IP-TRACKER${blue}"
+echo -e "${green}➣ ${cyan}[14]◉ King-Pshing${blue}"
+echo -e "${green}➣ ${cyan}[15]◉ Termux-Temaa${blue}"
+echo -e "${purple}======================================"
+echo -e "${red} Silahkan Pilihlah Menu TOOLSV7 [ 😈🔥]"
+echo -e "${purple}======================================"
+
+# Menu
+echo
+sleep 1
+read -p "Silahkan Pilihlah Menu [ 👹🔥 ]=" kuy
+
+# Logika Menu
+case $kuy in
+  1)
+    clear
+    sleep 1
+    cd 
+echo -e "${yellow}======================="
+echo -e "${red} Sedang Menginstall Tools!!!"
+echo -e "${yellow}========================"
+    git clone https://github.com/Sxp-ID/Brutal-W4
+    cd Brutal-W4
+    make install
+    ./main
+    ;;
+  2)
+    clear
+    sleep 2
+    cd
+echo -e "${yellow}======================="
+echo -e "${red} Sedang Menginstall Tools!!!"
+echo -e "${yellow}======================="
+    git clone https://github.com/HanzzAltar/Spam-pairing-V1
+    cd Spam-pairing-V1
+    node spam.js
+    ;;
+  3)
+    clear
+    sleep 3
+    cd
+echo -e "${yellow}======================="
+echo -e "${red} Sedang Menginstall Tools!!!"
+echo -e "${yellow}======================="
+    apt install nodejs git -y
+    git clone https://github.com/FlowFalcon/spam-pairing-wa
+    cd spam-pairing-wa
+    npm install
+    npm start
+    ;;
+  4)
+    clear
+    sleep 4
+    cd
+echo -e "${yellow}======================="
+echo -e "${red} Sedang Menginstall Tools!!!"
+echo -e "${yellow}======================="
+    git clone https://github.com/ZeltNamizake/spcwa
+    cd spcwa
+    npm start
+    ;;
+  5)
+    clear
+    sleep 5
+    cd
+echo -e "${yellow}========================"
+echo -e "${red} Sedang Menginstall Tools!!!"
+echo -e "${yellow}========================"
+    git clone https://github.com/Reyvknd/Spam-Pairing-Code.git
+    cd Spam-Pairing-Code
+    yarn install
+    npm start
+    ;;
+  6)
+    clear
+    sleep 6
+    cd
+echo -e "${yellow}========================"
+echo -e "${red} Sedang Menginstall Tools!!!"
+echo -e "${yellow}========================"
+    git clone https://github.com/Bayu12345677/OTP_TERROR.git
+    cd OTP_TERROR
+    make setup
+    make Run
+    ;;
+  7)
+    clear
+    sleep 7
+    cd
+echo -e "${yellow}========================"
+echo -e "${red} Sedang Menginstall Tools!!!"
+echo -e "${yellow}========================"
+    pip install colorama
+    pip2 install colorama
+    pip install requests
+    pip2 install requests
+    pip install phonenumbers
+    pip2 install phonenumbers
+    pip install datetime
+    pip2 install datetime
+    pip3 install --upgrade instaloader
+    pip install requests
+    pip install urllib
+    pip install colorama
+    pip install instaloader
+    pip install datetime
+    pip install phonenumbers as pnumb
+    pip install parse
+    pip install  geocoder
+    pip install carrier
+    pip install  timezone
+    pip install instaloader
+    git clone https://github.com/IccTeam/Owl-sint.git
+    cd Owl-sint
+    python owlsint.py
+    ;;
+  8)
+    clear 
+    sleep 8
+    cd
+echo -e "${yellow}========================="
+echo -e "${red} Sedang Menginstall Tools!!!"
+echo -e "${yellow}========================="
+    git clone https://github.com/HunxByts/GhostTrack.git
+    cd GhostTrack
+    pip3 install -r requirements.txt
+    python3 GhostTR.py
+    ;;
+  9)
+    clear 
+    sleep 9
+    cd
+echo -e "${yellow}========================="
+echo -e "${red} Sedang Menginstall Tools!!!"
+echo -e "${yellow}========================="
+    git clone --depth=1 https://github.com/htr-tech/zphisher.git
+
+    cd zphisher
+
+    bash zphisher.sh
+    ;;
+ 10)
+   clear 
+   sleep 10
+   cd 
+echo -e "${yellow}========================"
+echo -e "${red} Sedang Menginstall Tools!!!"
+echo -e "$yellow}========================="
+   pkg install golang
+   pkg install git 
+   git clone https://github.com/Mr-Pstar7/NIK-checker
+   cd NIK-checker
+   go build
+   chmod +x *
+   ./ktp-checker
+    ;;
+ 11)
+    clear
+    sleep 11
+    cd
+echo -e "${yellow}========================"
+echo -e "${red} Sedang Menginstall Tools!!!"
+echo -e"${red}==========================="
+    pkg install php
+    git clone https://github.com/Inject-ID/tiktok-followers
+    cd tiktok-followers
+    php bot.php
+    ;;
+  12)
+    clear
+    sleep 12
+    cd
+echo -e "${yellow}========================"
+echo -e "${cyan} Sedang Menginstall Tools!!!"
+echo -e "${yellow}========================"
+    git clone https://github.com/DRACULA-HACK/C-hacks
+
+    cd C-hacks
+
+    chmod +x chack.sh
+
+    ./chack.sh
+    ;;
+  13)
+    clear
+    sleep 13
+    cd
+echo -e "${yellow}============================"
+echo -e "${cyan} Sedang Menginstall Tools!!!"
+echo -e "${yellow}============================"
+    git clone https://github.com/Makky2693/Lacak-IP
+    cd Lacak-IP
+    python2 Lacak-IP.py
+    ;;
+  14)
+    clear
+    sleep 14
+    cd
+echo -e "${yellow}============================="
+echo -e "${cyan} Sedang Menginstall Tools!!!"
+echo -e "${yellow}============================="
+   git clone https://github.com/Ignitetch/AdvPhishing.git
+   cd AdvPhishing/
+   chmod 777 *
+   ./Android-Setup.sh
+   ./AdvPhishing.sh
+    ;;
+  15)
+    clear
+    sleep 15
+    cd
+echo -e "${yellow}=================================="
+echo -e "${cyan} Sedang Menginstall Tools!!!"
+echo -e "${yellow}=================================="
+    git clone https://github.com/Projeckerror/Tampilan.git
+    cdTampilan
+    python Tampilan.py
+     ;;
+
+
+  *)
+    echo -e "${red} Menu Tidak Ada Di Toolsv7!!!"
+    ;;
+esac
